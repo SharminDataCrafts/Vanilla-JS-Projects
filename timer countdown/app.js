1036f595d3918f39591ba26b7bc86d1ec084f276
SharminDataCrafts <sharminakter0411@gmail.com>
2024-11-20 23:04:57 +0600

    icon done

--- timer countdown/app.js
+++ timer countdown/app.js
@@ -1,31 +1,54 @@
+const topSections  = document.querySelectorAll('.top');
+const container = document.getElementsByClassName('container')
 const clock = document.querySelector('.clock');
 const alarm = document.querySelector('.alarm');
 const stopWatch = document.querySelector('.stop-watch');
 const timer = document.querySelector('.timer');
-const top = document.querySelector('.top');
+const iconTag = document.querySelector('i');
 
-
-clock.addEventListener('click',(e)=>{
-    let icon = e.currentTarget.firstElementChild
-    icon.style.fontWeight= '600';
-    icon.style.borderBottom = '1px solid black';
+topSections.forEach(topSection=>{
+    topSection.addEventListener('click',(e)=>{
+        let icon = e.currentTarget.firstElementChild
+        iconStyle(icon);
+        if(e.currentTarget.classList.contains('clock')){
+            showCurrentTime();
+        }
+    });
 });
 
-alarm.addEventListener('click',(e)=>{
-    let icon = e.currentTarget.firstElementChild
+function iconStyle(icon){
+    iconTag.style.fontWeight='500';
+    iconTag.style.borderBottom = 'none';
     icon.style.fontWeight= '600';
     icon.style.borderBottom = '1px solid black';
-});
+}
 
-stopWatch.addEventListener('click',(e)=>{
-    let icon = e.currentTarget.firstElementChild
-    icon.style.fontWeight= '600';
-    icon.style.borderBottom = '1px solid black';
-});
+function showCurrentTime(){
+    const currDate = new Date();
+    console.log(currDate.getTimezoneOffset())
+}
 
-timer.addEventListener('click',(e)=>{
-    let icon = e.currentTarget.firstElementChild
-    icon.style.fontWeight= '600';
-    icon.style.borderBottom = '1px solid black';
-});
+// clock.addEventListener('click',(e)=>{
+//     let icon = e.currentTarget.firstElementChild
+//     icon.style.fontWeight= '600';
+//     icon.style.borderBottom = '1px solid black';
+// });
+
+// alarm.addEventListener('click',(e)=>{
+//     let icon = e.currentTarget.firstElementChild
+//     icon.style.fontWeight= '600';
+//     icon.style.borderBottom = '1px solid black';
+// });
+
+// stopWatch.addEventListener('click',(e)=>{
+//     let icon = e.currentTarget.firstElementChild
+//     icon.style.fontWeight= '600';
+//     icon.style.borderBottom = '1px solid black';
+// });
+
+// timer.addEventListener('click',(e)=>{
+//     let icon = e.currentTarget.firstElementChild
+//     icon.style.fontWeight= '600';
+//     icon.style.borderBottom = '1px solid black';
+// });
 
